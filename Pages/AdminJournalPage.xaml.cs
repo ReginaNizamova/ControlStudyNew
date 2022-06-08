b11e1ed46e98f33041f665c1e9d2f00dbb739910
ReginaNizamova <nizamova-2001@bk.ru>
2022-06-08 20:23:43 +0500

    add filter admin

--- Pages/AdminJournalPage.xaml.cs
+++ Pages/AdminJournalPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace ControlStudy.Pages
 {
@@ -8,8 +10,24 @@ namespace ControlStudy.Pages
         public AdminJournalPage()
         {
             InitializeComponent();
-            ControlStudyEntities dataContext = new ControlStudyEntities();
-            dataGridAdmin.ItemsSource = dataContext.Sessions.ToList();
+
+            dataGridAdmin.ItemsSource = ControlStudyEntities.GetContext().Sessions.ToList();
+        }
+
+        private bool Filter(object item)
+        {
+            if (string.IsNullOrEmpty(filterText.Text))
+                return true;
+            else
+                return ((item as Session).DateSession.ToShortDateString().IndexOf(filterText.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    (item as Session).Person.Family.IndexOf(filterText.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void FilterTextTextChanged(object sender, TextChangedEventArgs e) //Фильтр
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(dataGridAdmin.Items);
+            view.Filter = Filter;
+            CollectionViewSource.GetDefaultView(dataGridAdmin.Items).Refresh();
         }
     }
 }
--- Pages/AdminUsersPage.xaml.cs
+++ Pages/AdminUsersPage.xaml.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Data;
 
 namespace ControlStudy.Pages
 {
@@ -46,6 +46,24 @@ namespace ControlStudy.Pages
                     MessageBox.Show(ex.Message.ToString());
                 }
             }    
+        }
+
+        private bool Filter(object item)
+        {
+            if (string.IsNullOrEmpty(filterText.Text))
+                return true;
+            else
+                return ((item as Person).Role.Role1.IndexOf(filterText.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    (item as Person).LoginUser.IndexOf(filterText.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    (item as Person).Family.IndexOf(filterText.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    (item as Person).Group.Group1.IndexOf(filterText.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void FilterTextTextChanged(object sender, TextChangedEventArgs e) //Фильтр
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(dataGridAdmin.Items);
+            view.Filter = Filter;
+            CollectionViewSource.GetDefaultView(dataGridAdmin.Items).Refresh();
         }
     }
 }
